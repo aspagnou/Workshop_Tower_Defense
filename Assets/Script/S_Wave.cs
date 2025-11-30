74b0eef8e7a08243d90f388193751e8e4c5a504a
Mathis Tollini <mathistol90@gmail.com>
2025-11-30 23:00:56 +0100

    Sequencer for each wave + Kill / Destroy all conditions

--- Assets/Script/S_Wave.cs
+++ Assets/Script/S_Wave.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ public class S_Wave : MonoBehaviour
 
     public Transform[] wayPoints;
 
-    public GameObject[] enemy;
+    //public GameObject[] enemy;
 
     public int numberOfEnemies = 5; //Number of enemies spawning in the wave
 
@@ -16,13 +17,23 @@ public class S_Wave : MonoBehaviour
 
     public int enemiesSpawned = 0; //Counter for Spawned enemies
 
-    public int nbrEnemiesKillCond = 0; //Checking how many enemies remains
+    public int nbrEnemiesKillCond; //Checking how many enemies remains
 
     public bool waveActive = false; //Check if the wave is active
 
     [SerializeField] public int delayBetweenWaveSetByThisWave;
 
+    [Header("Sequence")]
+    public Queue<S_Enemy> _enemyStack;
+    [SerializeField]private List<S_Enemy> _enemyList;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void OnEnable()
+    {
+        nbrEnemiesKillCond = _enemyList.Count;
+        _enemyStack = new Queue<S_Enemy>(_enemyList);
+    }
     void Start()
     {
         //Invoke("StartWave", 2);
@@ -46,11 +57,20 @@ public class S_Wave : MonoBehaviour
                 spawnTimer = spawnInterval;
             }
         }
+
+        if (nbrEnemiesKillCond <= 0) //enemiesSpawned >= numberOfEnemies &&
+        {
+            waveActive = false;
+            Debug.Log("Wave completed");
+            GetComponentInParent<S_WaveManager>().StartNextWave(delayBetweenWaveSetByThisWave);
+            gameObject.SetActive(false);
+        }
     }
 
     private void OneEnemyIsDead(S_Enemy deadEn)
     {
-
+        _enemyList.Remove(deadEn);
+        nbrEnemiesKillCond--;
     }
 
 
@@ -58,21 +78,21 @@ public class S_Wave : MonoBehaviour
 
     void SpawnEnemy()
     {
-        int rand = Random.Range(0, enemy.Length); // Randomely select an enemy type from the array
-        GameObject newEnemy = Instantiate(enemy[rand], transform.position, Quaternion.identity);
-        newEnemy.GetComponent<S_Enemy>().WayPoints = wayPoints;
+        //int rand = Random.Range(0, enemy.Length); // Randomely select an enemy type from the array
+        //GameObject newEnemy = Instantiate(enemy[rand], transform.position, Quaternion.identity);
+        //newEnemy.GetComponent<S_Enemy>().WayPoints = wayPoints;
         
+        S_Enemy enemyCandidate = _enemyStack.Dequeue();
+        S_Enemy enemyInstance = Instantiate(enemyCandidate, transform.position, Quaternion.identity);
+        enemyInstance.GetComponent<S_Enemy>().WayPoints = wayPoints;
+        enemyInstance.OnDead += OneEnemyIsDead;
+
+        _enemyList.Add(enemyInstance);
 
         enemiesSpawned++;
-        nbrEnemiesKillCond++;
+        
 
-        if (enemiesSpawned >= numberOfEnemies) //&& nbrEnemiesKillCond == 0
-        {
-            waveActive = false;
-            Debug.Log("Wave completed");
-            GetComponentInParent<S_WaveManager>().StartNextWave(delayBetweenWaveSetByThisWave);
-            gameObject.SetActive(false);
-        }
+        
 
     }
 
