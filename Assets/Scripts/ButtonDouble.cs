ef3ce3cd3cd1d6070c352ee809bd598c187fcb6c
Mathis Tollini <mathistol90@gmail.com>
2025-12-17 12:25:49 +0100

    minor changes on sfx and prefab ui

--- Assets/Scripts/ButtonDouble.cs
+++ Assets/Scripts/ButtonDouble.cs
@@ -95,7 +95,7 @@ public class ButtonDouble : MonoBehaviour
     {
         targetImage.sprite = pressedImage;
         buttonText.color = pressedColor;
-        _audioManager.PlayMusic(_audioManager.uiButton);
+        _audioManager.PlaySfx(_audioManager.uiButton);
 
 
     }
